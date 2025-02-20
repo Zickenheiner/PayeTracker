85a01af09d27cd2a295b28298a690bededcab495
Hakkenheiner <remi.zicken@gmail.com>
2025-02-20 15:14:53 +0100

    Initialize project

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,15 +3,6 @@ import express from "express";
 const router = express.Router();
 
 /* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
-// Define item-related routes
-import itemActions from "./modules/item/itemActions";
-
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
 
